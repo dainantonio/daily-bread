fa7847e3cad268202d43d1ca5fe53a8dd07f0118
dain russell <31678909+dainantonio@users.noreply.github.com>
2026-01-04 17:05:38 -0500

    Set base path to './' in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // An empty string here makes all paths relative. 
-  // It works no matter what your repo is named.
-  base: '', 
+  base: './',
 })
