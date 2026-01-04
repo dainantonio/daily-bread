7df2a29f7d06ecaa214ebfd22907c32f67f8da3c
dain russell <31678909+dainantonio@users.noreply.github.com>
2026-01-04 17:01:10 -0500

    Update base path in Vite config
    Change base path to an empty string for relative paths.

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANT: This must match your repository name exactly!
-  base: "/daily-bread/",
+  // An empty string here makes all paths relative. 
+  // It works no matter what your repo is named.
+  base: '', 
 })
